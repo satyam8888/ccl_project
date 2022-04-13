43518e44511d0cf03e0e2bf8de826494575daaac
satyam8888 <76624553+satyam8888@users.noreply.github.com>
2022-04-13 12:23:54 +0530

    Version Change

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def upload_key():
 		return 'Invalid File Format !'
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8000, debug=True)
-	#app.run()
+	#app.run(host='127.0.0.1', port=8000, debug=True)
+	app.run()
